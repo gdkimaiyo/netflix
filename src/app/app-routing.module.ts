21845253acc504c79a5e631eb76177e58f811b69
gdkimaiyo <gdkimaiyo@gmail.com>
2023-02-03 00:13:43 +0300

    add movies page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { ShowsComponent } from './pages/shows/shows.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ShowDetailsComponent } from './components/show-details/show-details.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { FavouritesComponent } from './pages/favourites/favourites.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'shows', component: ShowsComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'show/:id', component: ShowDetailsComponent },
+  { path: 'movies/favourites', component: FavouritesComponent },
   { path: '**', component: NotFoundComponent }, // 404 - Not Found page
 ];
 
