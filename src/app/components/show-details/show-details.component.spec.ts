9c43e8960e354e2a991bfd954f10ca9f29a25569
gdkimaiyo <gdkimaiyo@gmail.com>
2023-02-03 00:16:14 +0300

    add shows pages. show details

---
+++ src/app/components/show-details/show-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowDetailsComponent } from './show-details.component';
+
+describe('ShowDetailsComponent', () => {
+  let component: ShowDetailsComponent;
+  let fixture: ComponentFixture<ShowDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowDetailsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShowDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
