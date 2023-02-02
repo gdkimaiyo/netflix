485b427ee36ed8fb92454ea6c7ec4b47ab5c4185
gdkimaiyo <gdkimaiyo@gmail.com>
2023-02-03 00:15:13 +0300

    add a movie to favourites list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { FaqsComponent } from './components/faqs/faqs.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { MovieSummaryComponent } from './components/movie-summary/movie-summary.component';
+import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { ShowsComponent } from './pages/shows/shows.component';
+import { ShowDetailsComponent } from './components/show-details/show-details.component';
 
 registerLocaleData(en);
 
@@ -24,13 +29,19 @@ registerLocaleData(en);
     HomeComponent,
     FaqsComponent,
     MovieDetailsComponent,
-    MovieSummaryComponent
+    MovieSummaryComponent,
+    FavouritesComponent,
+    LoginComponent,
+    MoviesComponent,
+    ShowsComponent,
+    ShowDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
