f11a0a9c8210628989392c5fffac33ab150b9f77
gdkimaiyo <gdkimaiyo@gmail.com>
2023-03-06 16:50:48 +0300

    auth - login & logout feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mini-Netflix';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    const user = JSON.parse(localStorage.getItem("nflix_user") || '[]');
+    if (user && user?.length !== 0) {
+      this.authService.verifyUser({ token: user?.token, userId: user?._id })
+      .subscribe((res) => {
+        if (res?.data !== true) {
+          localStorage.removeItem("nflix_user");
+        }
+      },
+      (error) => {
+        localStorage.removeItem("nflix_user");
+      });
+    }
+  }
 }
